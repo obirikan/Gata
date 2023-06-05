78facc9e45ec23263d10dab87efa1a65576fed7b
obirikan <obirikan020@gmail.com>
2023-06-05 14:53:08 +0100

    new changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,24 +1,24 @@
 import React from "react";
 import PageWrapper from "../Layout/PageWrapper";
 import Hero from '../Components/Home/Hero'
-import WhyChooseRattify from "../Components/Home/WhyChooseRattify";
-import FairAndEthical from "../Components/Home/FairAndEthical";
-import ExploreThePower from "../Components/Home/ExploreThePower";
-import Enterprise from "../Components/Home/Enterprise";
-import GlobalScale from "../Components/Home/GlobalScale";
-import GetStarted from "../Components/Home/GetStarted";
-import OurSolutions from "../Components/Home/OurSolutions";
+// import WhyChooseRattify from "../Components/Home/WhyChooseRattify";
+// import FairAndEthical from "../Components/Home/FairAndEthical";
+// import ExploreThePower from "../Components/Home/ExploreThePower";
+// import Enterprise from "../Components/Home/Enterprise";
+// import GlobalScale from "../Components/Home/GlobalScale";
+// import GetStarted from "../Components/Home/GetStarted";
+// import OurSolutions from "../Components/Home/OurSolutions";
 const Home = () => {
   return (
     <PageWrapper>
       <Hero />
-      <WhyChooseRattify/>
+      {/* <WhyChooseRattify/>
       <OurSolutions/>
       <FairAndEthical/>
       <ExploreThePower/>
       <Enterprise/>
       <GetStarted/>
-      <GlobalScale/>
+      <GlobalScale/> */}
     </PageWrapper>
   );
 };
